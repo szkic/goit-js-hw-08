706b3fe76a386a0ca510cd3fecfbd9cdebd12b98
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-03-23 19:01:05 +0100

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+var throttle = require('lodash.throttle');
+
+function saveTime(data) {
+  console.log(data.seconds);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
